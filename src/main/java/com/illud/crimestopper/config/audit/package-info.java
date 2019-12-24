324628e27fa21049e4a66c9e670df63e10106e16
sahalr1 <mohammed.sahal@lxisoft.com>
2019-12-24 14:11:46 +0530

    Initial version of crimestopperGateway generated by JHipster-6.6.0

---
+++ src/main/java/com/illud/crimestopper/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.illud.crimestopper.config.audit;
---
+++ src/main/java/com/illud/crimestopper/repository/search/UserSearchRepository.java
@@ -0,0 +1,10 @@
+package com.illud.crimestopper.repository.search;
+
+import com.illud.crimestopper.domain.User;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * Spring Data Elasticsearch repository for the User entity.
+ */
+public interface UserSearchRepository extends ElasticsearchRepository<User, String> {
+}
